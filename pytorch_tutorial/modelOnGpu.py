b6e0234543920a29753a9779694427c698ff06d0
Keno <keno.0612.judo@icloud.com>
2018-08-11 10:40:34 +0900

    completed training claccifier

---
+++ pytorch_tutorial/modelOnGpu.py
@@ -0,0 +1,77 @@
+# load and normalize CIFAR10
+import torch
+import torchvision
+import torchvision.transforms as transforms
+%matplotlib inline
+#The output of torchvision datasets are PILImage images of range [0, 1].
+#We transform them to Tensors of normalized range [-1, 1].
+transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5) ,(0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                       download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                         shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                      download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                        shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# training on GPU
+# Define a convolution Neural network
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 *5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if device == "cuda:0"  and torch.cuda.device_count() > 1:
+    print("lets use", torch.cuda.device_count(), "GPUs!")
+    net = nn.DataParallel(net)
+print(device)
+net.to(device)
+for epoch in range(1000): # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        # make tensors runnable on GPU
+        inputs , labels = inputs.to(device), labels.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss:%.3f'  % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
